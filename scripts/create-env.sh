5a82f96c886a973db2981e74bf7119ddcdaf5e4a
Jan Dubois <jan.dubois@suse.com>
2018-04-19 17:17:08 -0700

    PGATS doesn't seem to support oauth to UAA; use local users for the director

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -15,13 +15,25 @@ bosh int "${TOPLEVEL}/src/bosh-deployment/bosh.yml" \
   -o "${TOPLEVEL}/src/bosh-deployment/jumpbox-user.yml" \
   -o "${TOPLEVEL}/src/bosh-deployment/uaa.yml" \
   -o "${TOPLEVEL}/src/bosh-deployment/credhub.yml" \
+  -o /dev/stdin \
   --vars-store "${WORKSPACE}/creds.yml" \
   -v director_name="Bosh Lite Director" \
   -v internal_ip=192.168.50.6 \
   -v internal_gw=192.168.50.1 \
   -v internal_cidr=192.168.50.0/24 \
   -v outbound_network_name=NatNetwork \
-  > "${WORKSPACE}/bosh.yml"
+  <<EOF > "${WORKSPACE}/bosh.yml"
+- type: replace
+  path: /instance_groups/name=bosh/properties/director/user_management
+  value:
+    provider: local
+    local:
+      users:
+      - name: admin
+        password: ((admin_password))
+      - name: hm
+        password: ((hm_password))
+EOF
 
 bosh create-env --state "${WORKSPACE}/state.json" "${WORKSPACE}/bosh.yml"
 
